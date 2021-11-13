ce98f035a352d148c692d5cd437e92a86bd9e54d
criiscz <criissf15@gmail.com>
2021-11-12 19:23:46 -0500

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,43 @@
+let numMessNormal = 0;
+const classMess = "GDhqjd";
+const classNameMess = "YTbUzc";
+const classNameContent = "oIy2qc";
+let lastMess = [];
+let numMess = 0;
+let isPause = false;
+
+function getLastMessages(numm) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(() => {
+                lastMess = [...document.getElementsByClassName(classMess)].slice(numm - numMess);
+                lastMess = lastMess.map((item) => {
+                    const name = item.getElementsByClassName(classNameMess)[0].innerText;
+                    const message = item.getElementsByClassName(classNameContent)[0].innerText;
+                    return {
+                        name: name,
+                        message: message
+                    }
+                })
+                numMessNormal = document.getElementsByClassName(classMess).length;
+                isPause = false;
+                return lastMess;
+            });
+        }, 10000);
+    })
+}
+
+function initBot() {
+    setInterval(() => {
+        numMess = document.getElementsByClassName(classMess).length;
+        if (numMess > numMessNormal && !isPause) {
+            isPause = true;
+            let numm = numMessNormal;
+            numMessNormal = numMess;
+            getLastMessages(numm).then((data) => {
+                dispatchMessage(data())
+            })
+        }
+    }, 1000);
+}
+
